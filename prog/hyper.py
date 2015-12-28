652bbe211f95777e91d7602b1bf3410f190765be
Mingli Yuan <mingli.yuan@gmail.com>
2015-12-28 10:19:24 +0800

    add skeleton for calculating poicare disk

--- prog/hyper.py
+++ prog/hyper.py
@@ -1,18 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# theta goes from 0 to 2pi
-theta = np.linspace(0, 2*np.pi, 1000)
 
-# the radius of the circle
-r = 1
+class PoincareDisk:
 
-# compute x1 and x2
-x1 = r*np.cos(theta)
-x2 = r*np.sin(theta)
+    def __init__(self):
+        fig, ax = plt.subplots(1)
+        ax.set_aspect(1)
+        self.axis = ax
+        self.figure = fig
 
-# create the figure
-fig, ax = plt.subplots(1)
-ax.plot(x1, x2)
-ax.set_aspect(1)
-plt.show()
+    def plot(self, shape):
+        shape.plot(self)
+        plt.show()
+
+    def mkBoundary(self):
+        return Boundary()
+
+    def mkPoint(self, x, y):
+        pass
+
+    def mkLine(self, x1, y1, x2, y2):
+        pass
+
+    def distanceBetween(self, x1, y1, x2, y2):
+        pass
+
+class Boundary:
+
+    def __init__(self):
+        self.t = np.linspace(0, 2 * np.pi, 1000)
+        self.x = np.cos(self.t)
+        self.y = np.sin(self.t)
+
+    def plot(self, pd):
+        pd.axis.plot(self.x, self.y)
+
+
+class Point:
+
+    def __init__(self):
+        pass
+
+
+class Line:
+
+    def __init__(self):
+        pass
