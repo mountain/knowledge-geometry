942540d45d5e8297b4dd31cd2656257160ff64ae
Mingli Yuan <mingli.yuan@gmail.com>
2016-03-17 08:06:53 +0800

    hyper improve

--- prog/hyper.py
+++ prog/hyper.py
@@ -7,8 +7,9 @@ pd = disk.instance
 
 pd.mkBoundary()
 
-p1 = pd.mkPoint(0.0, 1)
-p2 = pd.mkPoint(math.sqrt(0.5), math.sqrt(0.5))
+p0 = pd.mkPoint(0.0, 0.0)
+p1 = pd.mkPoint(0.0, 1.0)
+p2 = pd.mkPoint(1.0, 0.0)
 
 l1 = pd.mkLine(p1, p2)
 
