17f20b0baa6b313fa8e45565cb28fbe3a334a022
Mingli Yuan <mingli.yuan@gmail.com>
2019-09-12 02:07:14 +0800

    htree

--- prog/htree.py
+++ prog/htree.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from fractions import gcd
 from turtle import Turtle, Screen
 
 
 def drawLable(turtle, numerator, denominator):
-    turtle.forward(10)
-    turtle.write('%d/%d' % (numerator, denominator))
-    turtle.backward(10)
+    g = gcd(numerator, denominator)
+    if denominator != g:
+        turtle.write('%d/%d' % (numerator / g, denominator / g), font=("Arial", 8, "bold"))
+    else:
+        turtle.write('%d' % (numerator / g), font=("Arial", 8, "bold"))
 
 
 def drawHTree(turtle, order, center, length, numerator, denominator):
@@ -18,38 +21,40 @@ def drawHTree(turtle, order, center, length, numerator, denominator):
         turtle.up()
         turtle.goto(center)
         turtle.down()
+        turtle.right(90)
 
     drawLable(turtle, numerator, denominator)
 
     turtle.forward(length / 2)
-    drawLable(turtle, numerator * 2, denominator)
+    drawLable(turtle, numerator, denominator * 2)
     turtle.right(90)
     turtle.forward(length / 2)
     turtle.left(90)
+
+    drawHTree(turtle, order - 1, None, length / 2, numerator - denominator, denominator)
+
+    turtle.left(90)
+    turtle.forward(length)
+    turtle.right(90)
 
     drawHTree(turtle, order - 1, None, length / 2, numerator + denominator, denominator)
 
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.right(90)
-
-    drawHTree(turtle, order - 1, None, length / 2, numerator, denominator)
-
     turtle.right(90)
     turtle.forward(length / 2)
     turtle.right(90)
     turtle.forward(length)
+    drawLable(turtle, numerator * 2, denominator)
     turtle.left(90)
     turtle.forward(length / 2)
     turtle.right(90)
 
-    drawHTree(turtle, order - 1, None, length / 2, numerator, denominator)
+    drawHTree(turtle, order - 1, None, length / 2, numerator - denominator, denominator)
 
     turtle.right(90)
     turtle.forward(length)
     turtle.right(90)
 
-    drawHTree(turtle, order - 1, None, length / 2, numerator, denominator)
+    drawHTree(turtle, order - 1, None, length / 2, numerator + denominator, denominator)
 
     turtle.right(90)
     turtle.forward(length / 2)
