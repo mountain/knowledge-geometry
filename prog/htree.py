487c44d811108454e35cdd7b1dace47c8e136281
mingli <mingli.yuan@gmail.com>
2019-09-12 15:01:54 +0800

    add gutenberg

--- prog/htree.py
+++ prog/htree.py
@@ -12,61 +12,52 @@ def drawLable(turtle, numerator, denominator):
         turtle.write('%d' % (numerator / g), font=("Arial", 8, "bold"))
 
 
-def drawHTree(turtle, order, center, length, numerator, denominator):
+def drawHTree(turtle, order, length, numerator, denominator):
 
     if order < 0:
-        return
-
-    if center is not None:
-        turtle.up()
-        turtle.goto(center)
-        turtle.down()
+        drawLable(turtle, numerator, denominator)
+    else:
+        turtle.forward(length / 2)
         turtle.right(90)
-
-    drawLable(turtle, numerator, denominator)
-
-    turtle.forward(length / 2)
-    drawLable(turtle, numerator, denominator * 2)
-    turtle.right(90)
-    turtle.forward(length / 2)
-    turtle.left(90)
-
-    drawHTree(turtle, order - 1, None, length / 2, numerator - denominator, denominator)
-
-    turtle.left(90)
-    turtle.forward(length)
-    turtle.right(90)
-
-    drawHTree(turtle, order - 1, None, length / 2, numerator + denominator, denominator)
-
-    turtle.right(90)
-    turtle.forward(length / 2)
-    turtle.right(90)
-    turtle.forward(length)
-    drawLable(turtle, numerator * 2, denominator)
-    turtle.left(90)
-    turtle.forward(length / 2)
-    turtle.right(90)
-
-    drawHTree(turtle, order - 1, None, length / 2, numerator - denominator, denominator)
-
-    turtle.right(90)
-    turtle.forward(length)
-    turtle.right(90)
-
-    drawHTree(turtle, order - 1, None, length / 2, numerator + denominator, denominator)
-
-    turtle.right(90)
-    turtle.forward(length / 2)
-    turtle.left(90)
-    turtle.forward(length / 2)  # always leave the turtle where you found it!
+        turtle.forward(length / 2)
+        turtle.left(90)
+        drawHTree(turtle, order - 1, length / 2, numerator - denominator * 2, denominator * 2)
+        turtle.left(90)
+        turtle.forward(length / 2)
+        drawLable(turtle, numerator, denominator * 2)
+        turtle.forward(length / 2)
+        turtle.left(90)
+        drawHTree(turtle, order - 1, length / 2, numerator + denominator * 2, denominator * 2)
+        turtle.left(90)
+        turtle.forward(length / 2)
+        turtle.right(90)
+        turtle.forward(length / 2)
+        drawLable(turtle, numerator, denominator)
+        turtle.forward(length / 2)
+        turtle.right(90)
+        turtle.forward(length / 2)
+        turtle.left(90)
+        drawHTree(turtle, order - 1, length / 2, numerator * 2 + denominator, denominator)
+        turtle.left(90)
+        turtle.forward(length / 2)
+        drawLable(turtle, numerator * 2, denominator)
+        turtle.forward(length / 2)
+        turtle.left(90)
+        drawHTree(turtle, order - 1, length / 2, numerator * 2 - denominator, denominator)
+        turtle.left(90)
+        turtle.forward(length / 2)
+        turtle.right(90)
+        turtle.forward(length / 2)
 
     turtle.hideturtle()
 
 
 yertle = Turtle()
 
-drawHTree(yertle, 1, (0, 0), 512, 0, 1)
+yertle.goto(0, 0)
+yertle.right(90)
+
+drawHTree(yertle, 3, 256, 0, 1)
 
 screen = Screen()
 
