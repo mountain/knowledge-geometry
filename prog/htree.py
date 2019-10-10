56e0ab195d745716eee2f5d6f44ae89dc6b1891c
Mingli Yuan <mingli.yuan@gmail.com>
2019-10-11 01:17:02 +0800

    add notes

--- prog/htree.py
+++ prog/htree.py
@@ -6,16 +6,25 @@ from fractions import gcd
 from turtle import Turtle, Screen
 
 
-def drawLable(turtle, numerator, denominator):
+def drawLable(turtle, numerator, denominator, step=5):
     g = gcd(numerator, denominator)
+    turtle.penup()
+    turtle.right(90)
+    turtle.forward(step)
+    turtle.right(90)
+    turtle.forward(step)
     if denominator != g:
         turtle.write('%d/%d' % (numerator / g, denominator / g), font=("Arial", 8, "bold"))
     else:
         turtle.write('%d' % (numerator / g), font=("Arial", 8, "bold"))
+    turtle.backward(step)
+    turtle.left(90)
+    turtle.backward(step)
+    turtle.left(90)
+    turtle.pendown()
 
 
-def drawHTree(turtle, order, length, numerator, denominator):
-
+def drawHTree(turtle, order, length, numerator, denominator, shiftx=0, shifty=0):
     if order < 0:
         drawLable(turtle, numerator, denominator)
     else:
@@ -26,7 +35,7 @@ def drawHTree(turtle, order, length, numerator, denominator):
         drawHTree(turtle, order - 1, length / 2, numerator - denominator * 2, denominator * 2)
         turtle.left(90)
         turtle.forward(length / 2)
-        drawLable(turtle, numerator, denominator * 2)
+        drawLable(turtle, numerator, denominator * 2, step=10)
         turtle.forward(length / 2)
         turtle.right(90)
         drawHTree(turtle, order - 1, length / 2, numerator + denominator * 2, denominator * 2)
@@ -42,7 +51,7 @@ def drawHTree(turtle, order, length, numerator, denominator):
         drawHTree(turtle, order - 1, length / 2, numerator * 2 + denominator, denominator)
         turtle.right(90)
         turtle.forward(length / 2)
-        drawLable(turtle, numerator * 2, denominator)
+        drawLable(turtle, numerator * 2, denominator, step=0)
         turtle.forward(length / 2)
         turtle.left(90)
         drawHTree(turtle, order - 1, length / 2, numerator * 2 - denominator, denominator)
