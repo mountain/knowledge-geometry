bf43d624f4671854db1e2f0e74f5d2ba90e7e602
mingli <mingli.yuan@gmail.com>
2020-07-08 13:33:19 +0800

    add more

--- check.py
+++ check.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 e = np.e
-gamma = np.array([0.57721566490153286060651209008240243104215933593992], dtype=np.float128)[0] / 1.7616
+gamma = np.array([0.57721566490153286060651209008240243104215933593992], dtype=np.float128)[0]
+print(np.exp(gamma))
 
 elm_0 = 1
-elm_1 = gamma
+elm_1 = gamma * elm_0
 sum_inf = elm_0 + elm_1 / (1.0 - gamma)
 
 sum, prd = 0, 1
@@ -16,6 +17,6 @@ for i in range(10000):
     else:
         elm = elm * gamma
     sum = sum + elm
-    res = (1 + sum_inf - sum)
+    res = (elm_0 + (sum_inf - sum) / (sum_inf - elm_0))
     prd = prd * res
     print(elm, sum, res, prd)
