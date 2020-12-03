92f1fb2fed99719021b0901270c39fc538362b95
mingli <mingli.yuan@gmail.com>
2020-12-03 19:00:53 +0800

    add more

--- prog/hptrace.py
+++ prog/hptrace.py
@@ -57,8 +57,7 @@ ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 ax.set_xlim((-6 * 1.1, 6 * 1.1))
-ax.set_ylim((0, 6 * 1.1))
-
+ax.set_ylim((-6 * 1.1, 6 * 1.1))
 
 xs = np.ones([1], dtype=np.float128) + np.ones([1], dtype=np.float128) * 1.0j
 
@@ -75,13 +74,99 @@ def mk_cost(index):
     return cost
 
 
+def loopscope(idx):
+    if idx not in grid:
+        return 4500, 4500, 1
+
+    pos_start = grid[idx]['start']
+    pos_end = grid[idx]['end']
+    step = np.sign(pos_end - pos_start)
+    if step > 0:
+        pos_end = size
+    elif step < 0:
+        pos_end = 0
+    else:
+        step = 1
+    return pos_start, pos_end, step
+
+
+def trace0(central, branch):
+    cursor = np.ones([1], dtype=np.float128) * 1.0j
+    target = branch[np.argmin(np.abs(branch - cursor))]
+
+    idx_start = size // 2
+    r = np.abs(central - target)
+    idx_end = np.argmin(r)
+    if idx_start < idx_end:
+        idx_step = 1
+    else:
+        idx_step = -1
+
+    return trace(central, 1.0, '*', idx_start, idx_end, idx_step)
+
+
+def trace(branch, assignment, op, idx_start, idx_end, idx_step):
+    lastp, length = None, 0
+    dist, lastdist, llastdist = None, None, None
+    lastassignment = None
+    for sx in range(idx_start, idx_end, idx_step):
+        p = branch[sx]
+        if lastp is not None:
+            ds = np.sqrt((np.abs(p - lastp) ** 2) / (np.imag((p + lastp) / 2) ** 2))
+            if op == '+':
+                assignment = assignment + (ds / ratio * idx_step)
+            if op == '*':
+                assignment = assignment * np.exp(ds / ratio * idx_step)
+
+        dist = np.abs(p - xs)
+        #print('%s: %s, %s, %s' % (sx, p, dist, assignment))
+
+        if lastdist is not None and llastdist is not None:
+            if (dist - lastdist) * (lastdist - llastdist) < 0:
+                break
+
+        lastp = p
+        llastdist = lastdist
+        lastdist = dist
+        lastassignment = assignment
+
+    if op == '+':
+        return lastassignment, '*'
+    else:
+        return lastassignment, '+'
+
+
 for _ in range(arm.shape[0]):
-    res = minimize(mk_cost(_), 1.0, method='Powell')
+    cost = mk_cost(_)
+    res = minimize(cost, 1.0, method='Powell')
     scale = res.x
+    branches = arm * scale
+    center = branches.shape[0] // 2 - 1
+
+    print('----------------------------------')
     print(scale)
-    if scale[0] > 0:
-        branch = arm[_] * scale
-        assignment0 = np.exp(- np.log(scale) / np.log(ratio))
 
+    cursor = center
+    while cursor != _:
+        dirction = np.sign(_ - cursor)
+        idx_start, idx_end, idx_step = loopscope(cursor + dirction)
+        branch = branches[center + dirction]
 
+        if cursor == center:
+            assignment, op = trace0(arm[center], branch)
+        assignment, op = trace(branch, assignment, op, idx_start, idx_end, idx_step)
+        print('%s, %s: %s, %s' % (_, cursor, op, assignment))
 
+        ps = branch[idx_start:idx_end:idx_step]
+        ax.scatter(np.real(ps), np.imag(ps), s=1.0, c='red', marker='.')
+        xx = np.max(np.real(ps))
+        yx = np.max(np.imag(ps))
+        wsize = max(xx, yx)
+        ax.set_xlim((-wsize * 1.1, wsize * 1.1))
+        ax.set_ylim((-wsize * 1.1, wsize * 1.1))
+        adjust_aspect(fig, aspect=2)
+        fig.savefig('trace%02d.png' % cursor)
+
+        cursor += dirction
+
+    print(assignment)
--- prog/mk_arm.py
+++ prog/mk_arm.py
@@ -14,7 +14,7 @@ ratio = np.array([1.85501339 * np.pi], dtype=np.float128)
 
 
 angles = np.linspace(0, np.pi, size, dtype=np.float128)
-axis_m = np.zeros(size, dtype=np.float128) + np.linspace(0, size / 1000, size, dtype=np.float128) * 1j
+axis_m = np.power(ratio, np.linspace(- iteration, iteration, size, dtype=np.float128)) * 1j
 axis_a0 = (np.cos(angles) + np.sin(angles) * 1j)
 
 
@@ -40,6 +40,7 @@ def mk_arm(arclength):
         lastf = lastf / arclength
 
     arm = list(reversed(arm))
+    arm.append(axis_m)
 
     for _ in range(iteration):
         lasti = inv(lasti)
@@ -55,37 +56,38 @@ arm, fwd, inv = mk_arm(ratio)
 
 def validate():
     kx = None
-    for _ in range(2 * iteration):
+    for _ in range(2 * iteration + 3):
         print('validating %s ...' % _)
-        xs, ys = arm[_], arm[_ + 1]
-        a = xs.view(np.float128).reshape(size, 2)
-        b = ys.view(np.float128).reshape(size, 2)
-        dm = cdist(a, b, 'euclidean')
-        pos = np.argmin(dm)
-        ix, jx = pos // size, pos % size
-        d1 = dm[ix, jx]
-        d2 = np.abs(xs[ix] - ys[jx])
-        print('%s %s...' % (d1, d2))
-        assert d1 - d2 < 0.0001 and d1 < 0.001
-        k = np.real((xs[ix + 1] - xs[ix - 1]) / (ys[jx + 1] - ys[jx - 1]))
-        print('%s ...' % k)
-        assert k < 0.006
-        if kx is not None:
-            if kx > ix:
-                step = -1
-            else:
-                step = +1
-            lastp, length = None, 0
-            for sx in range(kx, ix, step):
-                p = xs[sx]
-                if lastp is not None:
-                    ds2 = (np.abs(p - lastp) ** 2) / (np.imag((p + lastp) / 2) ** 2)
-                    length += np.sqrt(ds2)
-                lastp = p
-            print('%s: d(%s, %s) = %s ...' % (_, kx, ix, length))
-            grid[_] = {'start': kx, 'end': ix, 'length': length}
+        if _ != iteration + 3:
+            xs, ys = arm[_], arm[_ + 1]
+            a = xs.view(np.float128).reshape(size, 2)
+            b = ys.view(np.float128).reshape(size, 2)
+            dm = cdist(a, b, 'euclidean')
+            pos = np.argmin(dm)
+            ix, jx = pos // size, pos % size
+            d1 = dm[ix, jx]
+            d2 = np.abs(xs[ix] - ys[jx])
+            print('%s %s...' % (d1, d2))
+            assert d1 - d2 < 0.0001 and d1 < 0.001
+            k = np.real((xs[ix + 1] - xs[ix - 1]) / (ys[jx + 1] - ys[jx - 1]))
+            print('%s ...' % k)
+            assert k < 0.006
+            if kx is not None:
+                if kx > ix:
+                    step = -1
+                else:
+                    step = +1
+                lastp, length = None, 0
+                for sx in range(kx, ix, step):
+                    p = xs[sx]
+                    if lastp is not None:
+                        ds2 = (np.abs(p - lastp) ** 2) / (np.imag((p + lastp) / 2) ** 2)
+                        length += np.sqrt(ds2)
+                    lastp = p
+                print('%s: d(%s, %s) = %s ...' % (_, kx, ix, length))
+                grid[_] = {'start': kx, 'end': ix, 'length': length}
 
-        kx = jx
+            kx = jx
 
 
 validate()
