e3b74ebdd1b699ccc267c784992cc87ba246b617
mingli <mingli.yuan@gmail.com>
2021-05-03 18:04:04 +0800

    add more translation

---
+++ epanl/build.sh
@@ -0,0 +1,23 @@
+rm *.acn
+rm *.alg
+rm *.bbl
+rm *.glg
+rm *.gls
+rm *.log
+rm *.pdf
+rm *.toc
+rm *.acr
+rm *.aux
+rm *.blg
+rm *.glo
+rm *.idx
+rm *.out
+rm *.xdy
+
+xelatex epanlz
+xelatex epanlz
+bibtex epanlz
+xelatex epanlz
+xelatex epanlz
+makeglossaries epanlz
+xelatex epanlz
